209f6cfe63b5a448313c406f126bb4f80c17eef0
gail@notgodzilla.com <A9HGb6lmpp!!##>
2023-05-01 21:42:49 -0400

    - Inject Retrofit and DealsApi - TODO: Unit tests - TODO: Loading spinner

--- app/src/main/java/com/target/targetcasestudy/api/DealsRepository.kt
+++ app/src/main/java/com/target/targetcasestudy/api/DealsRepository.kt
@@ -1,28 +1,11 @@
 package com.target.targetcasestudy.api
 
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.target.targetcasestudy.model.Product
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.create
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class DealsRepository {
-
-    private val dealsApi: DealApiKtx
-
-    init {
-        //Create instance of Moshi for JSON converter
-        val moshi = Moshi.Builder()
-            .add(KotlinJsonAdapterFactory())
-            .build()
-        //Create instance of Retrofit for networking
-        val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl("https://api.target.com/mobile_case_study_deals/v1/")
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .build()
-        dealsApi = retrofit.create()
-    }
+@Singleton
+class DealsRepository @Inject constructor(private val dealsApi: DealApiKtx) {
 
     suspend fun getDeals(): List<Product> = dealsApi.retrieveDeals().deals
 
--- app/src/main/java/com/target/targetcasestudy/injection/TargetDealsAppModule.kt
+++ app/src/main/java/com/target/targetcasestudy/injection/TargetDealsAppModule.kt
@@ -1,10 +1,16 @@
 package com.target.targetcasestudy.injection
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.target.targetcasestudy.api.DealApiKtx
 import com.target.targetcasestudy.api.DealsRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
 import javax.inject.Singleton
 
 @Module
@@ -12,6 +18,22 @@ import javax.inject.Singleton
 class TargetDealsAppModule {
 
     @Provides
+    fun provideRetrofit(): Retrofit {
+        //Create instance of Moshi for JSON converter
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+        //Create instance of Retrofit for networking
+        return Retrofit.Builder()
+            .baseUrl("https://api.target.com/mobile_case_study_deals/v1/")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+
+    @Provides
+    fun provideDealsAPI() = provideRetrofit().create<DealApiKtx>()
+
+    @Provides
     @Singleton
-    fun provideDealsRepository(): DealsRepository = DealsRepository()
+    fun provideDealsRepository(): DealsRepository = DealsRepository(provideDealsAPI())
 }
